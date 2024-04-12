93d99a6cf77fe24e247ac5ca682d141102d5e20f
alrincon <alrincon@unirioja.es>
2024-04-12 09:41:44 +0200

    Cambios

---
+++ include/R_z.h
@@ -0,0 +1,7 @@
+#ifndef _R_Z_
+#define _R_Z_
+#include "Matrix.h"
+
+Matrix R_z (double angle);
+
+#endif
---
+++ src/EKF_Tests.cpp
@@ -0,0 +1,41 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "../include/Matrix.h"
+#include "../include/R_z.h"
+#include "string.h"
+
+#define TOL_ 10e-14
+
+
+using namespace std;
+
+
+int tests_run = 0;
+
+#define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
+#define _assert(test) \
+  do {                \
+    if (!(test)) {    \
+      FAIL();         \
+      return 1;       \
+    }                 \
+  } while (0)
+#define _verify(test) \
+  do {                \
+    int r = test();   \
+    tests_run++;      \
+    if (r) return r;  \
+  } while (0)
+
+
+int R_z_01(){
+    double angle = 2.0;
+    Matrix sol(3,3);
+
+    sol = R_z(angle);
+    sol = R_z(angle);
+    sol.print();
+
+    _assert(fabs(sol(1,1)) -0.416146836547142 < TOL_ && fabs(sol(1,2)) - 0.909297426825682 < TOL_ && fabs(sol(1,3)) < TOL_);
+}
