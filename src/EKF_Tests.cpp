24e46fb3eda9f69d4551050e24c1fc6bfbb77990
alrincon <alrincon@unirioja.es>
2024-04-17 16:20:20 +0200

    Testststststst

--- src/EKF_Tests.cpp
+++ src/EKF_Tests.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 #include "../include/Matrix.h"
+#include "../include/R_x.h"
+#include "../include/R_y.h"
 #include "../include/R_z.h"
 #include "string.h"
 
@@ -28,18 +30,59 @@ int tests_run = 0;
     if (r) return r;  \
   } while (0)
 
+int R_x_01(){
+    double angle = 2.0;
+    Matrix sol(3,3);
+
+    sol = R_x(angle);
+
+    _assert(fabs(sol(1,1) -1) < TOL_ && fabs(sol(1,2)) < TOL_ && fabs(sol(1,3)) < TOL_);
+    _assert(fabs(sol(2,1)) < TOL_ && fabs(sol(2,2) + 0.416146836547142) < TOL_ && fabs(sol(2,3) - 0.909297426825682) < TOL_);
+
+    return 0;
+}
+
+int R_y_01(){
+    double angle = 2.0;
+    Matrix sol(3,3);
+
+    sol = R_y(angle);
+
+    _assert(fabs(sol(1,1) + 0.416146836547142) < TOL_ && fabs(sol(1,2)) < TOL_ && fabs(sol(1,3) + 0.909297426825682) < TOL_);
+
+    return 0;
+}
 
 int R_z_01(){
     double angle = 2.0;
     Matrix sol(3,3);
 
     sol = R_z(angle);
-    sol = R_z(angle);
-    sol.print();
 
-    _assert(fabs(sol(1,1)) -0.416146836547142 < TOL_ && fabs(sol(1,2)) - 0.909297426825682 < TOL_ && fabs(sol(1,3)) < TOL_);
+    _assert(fabs(sol(1,1) + 0.416146836547142) < TOL_ && fabs(sol(1,2) - 0.909297426825682) < TOL_ && fabs(sol(1,3)) < TOL_);
+
+    return 0;
+}
+
+
+
+int all_tests(){
+
+    _verify(R_x_01);
+    _verify(R_y_01);
+    _verify(R_z_01);
+
+    return 0;
 }
 
 int main(){
-    _verify(R_z_01);
+    int result = all_tests();
+
+    if(result == 0){
+        printf("PASSED\n");
+    }
+
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
 }
