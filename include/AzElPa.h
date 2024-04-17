0658165d5c25d3451c6d5d097ebe3612096df772
lamorer <lamorer@unirioja.es>
2024-04-17 16:48:09 +0200

    AzElPa

---
+++ include/AzElPa.h
@@ -0,0 +1,10 @@
+#ifndef AZELPA_H
+#define AZELPA_H
+
+#include <cmath>
+#include <vector>
+
+void AzElPa(double s[], double& Az, double& El, std::vector<double>& dAds, std::vector<double>& dEds);
+
+
+#endif // AZELPA_H
